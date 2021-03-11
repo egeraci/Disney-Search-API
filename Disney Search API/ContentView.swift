43030d44534851be9ed1146c4bd78bd2d9363eab
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-11 16:00:13 -0600

    Started Disney API works with alert notification

--- Disney Search API/ContentView.swift
+++ Disney Search API/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var entrys = [Entry]()
+    @State private var showingalert = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView
+        {
+            List(entrys)
+            {
+                entry in
+                Link(destination: URL(string: entry.link)!, label:
+                {
+                    Text(entry.title)
+                })
+            }
+            .navigationTitle("Disney!")
+        }
+        .onAppear(perform:
+                    {
+                        getDisney()
+                    })
+        .alert(isPresented: $showingalert, content: {
+            Alert(title: Text("loading error"), message: Text("There's a problem loading your data"), dismissButton: .default(Text("OK")))
+        })
+    }
+    func getDisney()
+    {
+        showingalert = true
     }
 }
 
@@ -18,4 +41,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Entry: Identifiable
+{
+    let id = UUID()
+    let title: String
+    let link: String
+    
 }

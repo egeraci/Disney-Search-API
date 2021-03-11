07e1206ca360caca9bc46e21527902949ea12f6a
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-11 15:29:49 -0600

    Initial Commit

---
+++ Disney Search API/Disney_Search_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_Search_APIApp.swift
+//  Disney Search API
+//
+//  Created by Student on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Disney_Search_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d7c5a00c9194a7774c1f9293aae9bac4a5485589
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-16 16:07:16 -0500

    I'm almost done! Some reason somethings not working.

--- Disney Search API/ContentView.swift
+++ Disney Search API/ContentView.swift
@@ -8,31 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State private var entrys = [Entry]()
+    @State private var entries = [Entry]()
     @State private var showingalert = false
     var body: some View {
         NavigationView
         {
-            List(entrys)
-            {
+            List(entries) {
                 entry in
-                Link(destination: URL(string: entry.link)!, label:
-                {
+                Link(destination: URL(string: entry.link)!, label: {
                     Text(entry.title)
                 })
             }
             .navigationTitle("Disney!")
         }
-        .onAppear(perform:
-                    {
-                        getDisney()
-                    })
+        .onAppear(perform: {
+            getDisney()
+        })
         .alert(isPresented: $showingalert, content: {
             Alert(title: Text("loading error"), message: Text("There's a problem loading your data"), dismissButton: .default(Text("OK")))
         })
     }
+    
     func getDisney()
     {
+        let apiKey = "?rapidapi-key=9ffc0481e6msh0b0a5178fa7b7a1p135476jsn3edbce5e740f"
+        let query = "https://google-search3.p.rapidapi.com/api/v1/search/q=disney&num=100\(apiKey)"
+        if let url = URL (string:query)
+        {
+            if let data = try? Data(contentsOf: url)
+            {
+                let json = try! JSON(data: data)
+                let contents = json["resalts"].arrayValue
+                for item in contents
+                {
+                    let title = item["title"].stringValue
+                    let link = item ["link"].stringValue
+                    let entry = Entry(title: title,link: link)
+                    entries.append(entry)
+                }
+                return
+            }
+        }
         showingalert = true
     }
 }
